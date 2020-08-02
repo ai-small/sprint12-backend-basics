4507c39254f9a9a3749108a474ee6d7dfa7dbcd4
ai-small <aismall@yandex.ru>
2020-08-03 00:21:02 +1200

    404 error handled

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const path = require('path');
 const router = require('./routes/users');
+const cardsRouter = require('./routes/cards');
+const notFound = require('./routes/notFound');
 
 const app = express();
 const { PORT = 3000 } = process.env;
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', router);
+app.use('/', cardsRouter);
+app.use(notFound);
 
 app.listen(PORT, () => {
 
