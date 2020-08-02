b08e5b746278dadb745accb9883eec252b995a5d
ai-small <aismall@yandex.ru>
2020-08-02 23:41:44 +1200

    Configured routing for /users and /users/:id. Added users.json and cards.json

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const path = require('path');
+const router = require('./routes/users');
 
 const app = express();
 const { PORT = 3000 } = process.env;
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/', router);
 
 app.listen(PORT, () => {
 
